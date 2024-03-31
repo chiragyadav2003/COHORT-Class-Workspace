ca782c0dd6c159441f72b23eff22257464066484
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-01 01:15:57 +0530

    init common for code sharing b/w apps

--- week-16/turbo-v3/apps/backend/src/index.js
+++ week-16/turbo-v3/apps/backend/src/index.js
@@ -13,3 +13,4 @@ app.get("/", (req, res) => {
 app.listen(3004, () => {
     console.log("backend is running on port - 3004");
 });
+//*NOTE: in tsconfig.json file we will remove (outdir:"./dist") to remove error
---
+++ week-16/turbo-v3/packages/common/src/index.ts
@@ -0,0 +1,3 @@
+export const value = "Chirag Yadav"
+
+export const BACKEND_URL = "https://google.com"
