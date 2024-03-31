dca79451f097cb0d4f25e10bb775455a6bb4267a
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-01 01:37:41 +0530

    configuring tsconfigs and build and dev commands using "esbuild"

--- week-16/turbo-v3/apps/backend/src/index.ts
+++ week-16/turbo-v3/apps/backend/src/index.ts
@@ -13,3 +13,5 @@ app.listen(3004, ()=>{
 })
 
 //*NOTE: in tsconfig.json file we will remove (outdir:"./dist") to remove error
+
+//NOTE: "dev": "tsc -b && node dist/index.js" remove this build command and esbuild
