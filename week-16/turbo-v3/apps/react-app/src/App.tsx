102b2dc2b889f5b0b0a9fbf610b625f5787ac58c
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-03-31 19:02:35 +0530

    adding button in react app

--- week-16/turbo-v3/apps/react-app/src/App.tsx
+++ week-16/turbo-v3/apps/react-app/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Button} from "@repo/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <Button appName={'ReactApp'}>Hi, there 👽</Button>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
