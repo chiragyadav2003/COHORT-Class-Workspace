02abc22cd767f1fdabfdf221c40f3d77d8d3ede1
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-01 18:48:11 +0530

    init next app for nextauth

---
+++ week-16/next-auth/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      <h2>NextAuth app home page</h2>
+    </div>
+  );
+}
