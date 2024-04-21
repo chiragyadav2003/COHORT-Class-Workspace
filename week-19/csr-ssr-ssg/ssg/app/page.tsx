2ad1357ad91a7f30c7b628ea31baa47734d43fb8
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-22 00:40:18 +0530

    stataic side generation

---
+++ week-19/csr-ssr-ssg/ssg/app/page.tsx
@@ -0,0 +1,23 @@
+
+export default async function Home() {
+  const res = await fetch("https://sum-server.100xdevs.com/todos")
+  const data = await res.json()
+  console.log(JSON.stringify(data))
+  return (
+    <div>
+      Hello, i am from static side generation
+      <div>
+        {
+          data.todos.map((todo: any) => {
+            return (
+              <div key={todo.id}>
+                {todo.title}
+                {todo.description}
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
