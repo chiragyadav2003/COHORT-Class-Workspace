c45aaa67e180ae188b62623cff03cf441c59bd00
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-06-13 21:18:12 +0530

    setup docs route

--- week-20/20.2-OpenAPI Spec Offline/02-honojs/src/index.ts
+++ week-20/20.2-OpenAPI Spec Offline/02-honojs/src/index.ts
@@ -1,9 +1,44 @@
-import { OpenAPIHono } from '@hono/zod-openapi'
+import { createRoute, OpenAPIHono } from '@hono/zod-openapi';
+import { ParamsSchema } from './inputs';
+import { UserSchema } from './outputs';
 
 const app = new OpenAPIHono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const getUserRoute = createRoute({
+  method: "get",
+  path: "/users/{id}",
+  request: {
+    params: ParamsSchema
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: UserSchema
+        },
+      },
+      description: "Get user details."
+    }
+  }
+})
+
+app.openapi(getUserRoute, (c) => {
+  const { id } = c.req.valid('param');
+  return c.json({
+    id,
+    age: 20,
+    name: "Ultraman"
+  })
+})
+
+
+// The OpenAPI documentation will be available at /doc
+app.doc('/doc', {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0.0',
+    title: 'My API',
+  },
 })
 
 export default app
