d91839f954a4e73b2867a74237069ce10aa590ad
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-08-26 13:00:24 +0530

    testing multiplication function

--- week-24/02 - testing in MERN stack/v1/src/tests/index.test.ts
+++ week-24/02 - testing in MERN stack/v1/src/tests/index.test.ts
@@ -1,15 +1,29 @@
 import { describe, expect, it } from '@jest/globals';
-import { sum } from '../index';
+import { sum, multiply } from '../index';
 
-describe('Testing sum function', () => {
+describe('Testing all the calculator functionality:', () => {
 
-  it('should sum 1 and 2 correctly', () => {
-    const finalAns = sum(1, 2);
-    expect(finalAns).toBe(3);
+  describe('Testing sum function', () => {
+    it('should sum 1 and 2 correctly', () => {
+      const finalAns = sum(1, 2);
+      expect(finalAns).toBe(3);
+    });
+
+    it('should sum negative numbers correctly', () => {
+      expect(sum(-1, -2)).toBe(-3);
+    })
   });
 
-  it('should sum negative numbers correctly', () => {
-    expect(sum(-1, -2)).toBe(-3);
-  })
+
+  describe('testing multiplication function', () => {
+    it('should multiply 1 and 2 correctly', () => {
+      expect(multiply(1, 3)).toBe(3);
+    });
+
+    it('should multiply -1 and -2 correctly', () => {
+      expect(multiply(-1, -2)).toBe(2);
+    });
+  });
+
 
 });
